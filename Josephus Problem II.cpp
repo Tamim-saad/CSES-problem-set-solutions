c09765dc69aa76dbb13d377ed02b27e97886dd46
Tamim Hasan Saad <tamimsaad1812@gmail.com>
2025-03-03 14:29:56 +0600

    Improve code readability

--- Josephus Problem II.cpp
+++ Josephus Problem II.cpp
@@ -1,95 +1,20 @@
-/// say Alhamdulillah
-
+/// Alhamdulillah
 #include <bits/stdc++.h>
 using namespace std;
-
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
 
 // template <typename Key, typename Mapped, typename Compare, typename Tag,
 //           typename Policy>
 // class tree;
-typedef tree<int, null_type, less<int>, rb_tree_tag,
-             tree_order_statistics_node_update>
-    indexed_set;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
 
-#define fast                                                                   \
-  ios_base::sync_with_stdio(false);                                            \
+#define fast                        \
+  ios_base::sync_with_stdio(false); \
   cin.tie(0);
-// cout.tie(0);
 #define ll long long
-// #define init             \
-//     ll count = 0, zz, t; \
-//     cin >> t;            \
-//     while (t--)
-#define ld long double
-#define ed "\n"
-#define eb emplace_back
-#define con continue
-#define bre break
-#define pl " "
-#define pll " --- "
-// #define size size()
-#define mpr make_pair
-#define vec vector<ll>
-#define lmax LONG_LONG_MAX
-#define lmin LONG_LONG_MIN
-#define forr(i, a, b) for (ll i = a; i < b; i++)
-#define forrev(i, a, b) for (ll i = a; i >= b; i--)
-
-// #define v.all v.begin(), v.end()
-
-#define printArrWhole(arr, n)                                                  \
-  for (ll i = 0; i < n; i++)                                                   \
-    cout << arr[i] << " ";                                                     \
-  cout << endl;
-
-#define printArrLim(arr, in, n)                                                \
-  for (ll i = in; i <= n; i++)                                                 \
-    cout << arr[i] << " ";                                                     \
-  cout << endl;
-
-#define printYesNo(flag)                                                       \
-  if (flag == 0)                                                               \
-    cout << "NO";                                                              \
-  else                                                                         \
-    cout << "YES";                                                             \
-  cout << endl;
-
-#define printvcWhole(vc)                                                       \
-  for (auto x : vc)                                                            \
-    cout << x << " ";                                                          \
-  cout << endl;
-
-#define printvcLim(vc, in, n)                                                  \
-  for (ll i = in; i <= n; i++)                                                 \
-    cout << vc[i] << " ";                                                      \
-  cout << endl;
-
-#define printdqWhole(dq)                                                       \
-  for (auto x : dq)                                                            \
-    cout << x << " ";                                                          \
-  cout << endl;
-
-#define printdqLim(dq, in, n)                                                  \
-  for (ll i = in; i <= n; i++)                                                 \
-    cout << dq[i] << " ";                                                      \
-  cout << endl;
-
-#define sortArr(arr, n) sort(arr, arr + n);
-#define sortvc(v) sort(v.begin(), v.end());
-#define rev(v) reverse(v.begin(), v.end());
-// #define find(v, x) find(v.begin(), v.end(), x);
-
-ll returnMod(ll a, ll b) { return (a % b); }
-
-// ll getJumpPos(ll i,ll j, ll setSize)
-// {
-
-// }
 
 int main() {
-
   ll n, ans, j;
   cin >> n >> j;
 
@@ -102,14 +27,10 @@ int main() {
   }
 
   int ind = j % st.size();
-
   while (!st.empty()) {
     int killed = *st.find_by_order(ind);
     cout << killed << " ";
     st.erase(killed);
-    if (!st.empty())
-      ind = ((ind % st.size() + j) % st.size());
+    if (!st.empty()) ind = ((ind % st.size() + j) % st.size());
   }
-
-  return 0;
 }
